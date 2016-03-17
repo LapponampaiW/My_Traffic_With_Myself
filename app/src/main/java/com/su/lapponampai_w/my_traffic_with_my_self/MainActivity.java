d8a7001829fa7473a54abd0665931599f7fea90d
ball <ballz_v@hotmail.com>
2016-03-17 15:31:39 +0700

    first commit

---
+++ app/src/main/java/com/su/lapponampai_w/my_traffic_with_my_self/MainActivity.java
@@ -0,0 +1,13 @@
+package com.su.lapponampai_w.my_traffic_with_my_self;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
