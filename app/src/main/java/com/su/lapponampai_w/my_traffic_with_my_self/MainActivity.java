9746df27f60c437c9e51d619df67fd6ae7a9ffe1
ball <ballz_v@hotmail.com>
2016-03-18 00:54:02 +0700

    going to create My_Adapter

--- app/src/main/java/com/su/lapponampai_w/my_traffic_with_my_self/MainActivity.java
+++ app/src/main/java/com/su/lapponampai_w/my_traffic_with_my_self/MainActivity.java
@@ -1,13 +1,51 @@
 package com.su.lapponampai_w.my_traffic_with_my_self;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttom_about;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindwidget();
+
+
+
+        press_open_hotmail();
+
+
+
+    }
+
+    private void press_open_hotmail() {
+
+        buttom_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.hotmail.com"));
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+
+    private void bindwidget() {
+
+        buttom_about = (Button) findViewById(R.id.main_bottom);
+
+
     }
 }
---
+++ app/src/main/java/com/su/lapponampai_w/my_traffic_with_my_self/My_Adapter.java
@@ -0,0 +1,11 @@
+package com.su.lapponampai_w.my_traffic_with_my_self;
+
+/**
+ * Created by apple on 3/18/16.
+ */
+public class My_Adapter {
+
+
+
+
+}
